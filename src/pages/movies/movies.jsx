b80610865f8f0cb00a7859678fc000d1ceedecbd
Oleksii Shevchenko <vazerius@gmail.com>
2022-11-02 23:24:02 +0200

    +

--- src/pages/movies/movies.jsx
+++ src/pages/movies/movies.jsx
@@ -1,32 +1,35 @@
 import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { FormSearch } from 'components/searchForm/formSearch';
 import { searchApiMovie } from 'api/moviApi';
 import { HomeList } from 'components/homeList/homeList';
-export const Movies = () => {
-  const [searchValue, setSearchValue] = useState('');
-  const [itemsData, setItemsData] = useState([]);
-  useEffect(() => {
-    if (searchValue === '') {
-      return;
-    }
 
+export const Movies = () => {
+  // const [searchValue, setSearchValue] = useState('');
+  const [itemsData, setItemsData] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') ?? '';
+  console.log(query);
+  useEffect(() => {
+    console.log('asdas');
     const respSearchMovie = async () => {
       try {
-        console.log();
-        const resp = await searchApiMovie(searchValue);
-        console.log(resp);
+        const resp = await searchApiMovie(query);
         if (resp.length > 0) {
           setItemsData(resp);
         }
       } catch (error) {}
     };
-    respSearchMovie(searchValue);
-  }, [searchValue]);
+    respSearchMovie(query);
+  }, [searchParams]);
 
-  const onSubmit = value => {
-    setSearchValue(value);
+  const onSubmit = query => {
+    const nextParams = query !== '' ? { query } : {};
+
+    setSearchParams(nextParams);
+    // setSearchValue(query);
   };
-  console.log(searchValue);
+  console.log(itemsData);
   const onClickId = e => {
     console.log(e.currentTarget.id);
   };
