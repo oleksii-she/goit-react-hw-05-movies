49c9e8b98989b05c3c958adf8847de5d2139906c
Oleksii Shevchenko <vazerius@gmail.com>
2022-11-05 22:59:10 +0200

    +

--- src/pages/movies/movies.jsx
+++ src/pages/movies/movies.jsx
@@ -3,8 +3,10 @@ import { useSearchParams } from 'react-router-dom';
 import { FormSearch } from 'components/searchForm/formSearch';
 import { searchApiMovie } from 'api/moviApi';
 import { HomeList } from 'components/homeList/homeList';
+import { RejectedId } from 'components/rejected/rejected';
 
 const Movies = () => {
+  const [status, setStatus] = useState('idle');
   const [itemsData, setItemsData] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get('query') ?? '';
@@ -16,15 +18,21 @@ const Movies = () => {
         if (resp.length > 0) {
           setItemsData(resp);
         }
-      } catch (error) {}
+      } catch (error) {
+        setStatus('rejected');
+      }
     };
     respSearchMovie();
-  }, [searchParams]);
+  }, [query]);
 
   const onSubmit = query => {
     const nextParams = query !== '' ? { query } : {};
     setSearchParams(nextParams);
   };
+
+  if (status === 'rejected') {
+    return <RejectedId />;
+  }
 
   return (
     <>
