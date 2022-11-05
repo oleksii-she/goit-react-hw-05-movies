4e535265062b9a0f213badf7fadc5db5a17af6f1
oleksii-she <94850347+oleksii-she@users.noreply.github.com>
2022-11-05 21:00:18 +0000

    Deploying to gh-pages from @ oleksii-she/goit-react-hw-05-movies@49c9e8b98989b05c3c958adf8847de5d2139906c 🚀

---
+++ static/js/365.0abf311a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[365],{3365:function(e,r,n){n.r(r),n.d(r,{default:function(){return d}});var t,c,o=n(168),a=n(6444),i=a.ZP.div(t||(t=(0,o.Z)(["\n  display: flex;\n  justify-content: center;\n"]))),u=a.ZP.h1(c||(c=(0,o.Z)(["\n  padding: 300px;\n"]))),l=n(184),d=function(){return(0,l.jsx)(i,{children:(0,l.jsx)(u,{children:"Sorry, an error occurred, try reloading the page, or click Home"})})}}}]);
+//# sourceMappingURL=365.0abf311a.chunk.js.map
